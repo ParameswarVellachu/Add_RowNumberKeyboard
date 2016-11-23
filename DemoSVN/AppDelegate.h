693fb9b6468e5529bdaededd1b8e483de8d34a2d
Parameswaran Vellachuclear <appsdeveloperinfo@gmail.com>
2016-11-23 10:59:11 +0530

    Initial Commit

---
+++ DemoSVN/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoSVN
+//
+//  Created by MTCHNDT on 23/11/16.
+//  Copyright © 2016 MTPL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoSVN/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DemoSVN
+//
+//  Created by MTCHNDT on 23/11/16.
+//  Copyright © 2016 MTPL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
